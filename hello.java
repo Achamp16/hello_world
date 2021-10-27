b8dab8aef6653a902dabe44e459d7c1bcce75f61
Achamp16 <achampion618@gmail.com>
2021-10-27 13:15:54 -0400

    Update hello.java

--- hello.java
+++ hello.java
@@ -5,11 +5,11 @@ public class hello {
     public static void main(String [] args) {
 
        // initialize variables
-       String firstName = "Alex";
+       String firstName = "Alec";
 	
  	   // output welcome message
-       System.out.println("Change this to a welcome message using " + 
-                          "the variable firstName");
+       System.out.println("Hello, my name is " + 
+                          firstName + ".");
 	 
 
     }
